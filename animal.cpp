3fab0755754955910f8727eb087189c9df497bda
Randy Heiland <heiland@indiana.edu>
2019-12-29 09:17:12 -0500

    update

--- animal.cpp
+++ animal.cpp
@@ -8,6 +8,16 @@
 // ['Animal', 'Dog', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'call_go']
 // >>> 
 
+//
+// ~/git/PyPhysiCell$ c++ -O3 -Wall -shared -std=c++11 -undefined dynamic_lookup `python3 -m pybind11 --includes` animal.cpp -o animal`python3-config --extension-suffix`
+// >>> import animal
+// In [5]: d=animal.Dog()
+// In [9]: d.go(5)
+// Out[10]: 'woof! woof! woof! woof! woof! '
+// In [3]: d.get_weight()
+// 42
+
+#include <iostream>
 
 #include <pybind11/pybind11.h>
 
@@ -20,6 +30,8 @@ public:
 };
 
 class Dog : public Animal {
+private:
+    float weight = 42.;
 public:
     std::string go(int n_times) override {
         std::string result;
@@ -27,18 +39,25 @@ public:
             result += "woof! ";
         return result;
     }
+    void get_weight() {
+        std::cout << this->weight << std::endl;
+    }
 };
+
+Dog mali;
 
 std::string call_go(Animal *animal) {
     return animal->go(3);
 }
 
-PYBIND11_MODULE(example, m) {
+// PYBIND11_MODULE(example, m) {
+PYBIND11_MODULE(animal, m) {
     py::class_<Animal>(m, "Animal")
         .def("go", &Animal::go);
 
     py::class_<Dog, Animal>(m, "Dog")
-        .def(py::init<>());
+        .def(py::init<>())
+        .def("get_weight", &Dog::get_weight);
 
     m.def("call_go", &call_go);
 }
